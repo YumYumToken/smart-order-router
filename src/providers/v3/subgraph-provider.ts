cf8f02986a9e99ded5194549852d0cb6fff32c94
YumYumToken <yumyumtoken@proton.me>
2023-04-20 02:08:04 +0800

    update version

--- src/providers/v3/subgraph-provider.ts
+++ src/providers/v3/subgraph-provider.ts
@@ -61,6 +61,8 @@ const SUBGRAPH_URL_BY_CHAIN: { [chainId in ChainId]?: string } = {
     'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-gorli',
   [ChainId.BSC]:
     'https://api.thegraph.com/subgraphs/name/ilyamk/uniswap-v3---bnb-chain',
+  [ChainId.BASE_GOERLI]:
+    'https://api.studio.thegraph.com/query/45375/yumyumswap-subgraph/v0.1.5',
 };
 
 const PAGE_SIZE = 1000; // 1k is max possible query size from subgraph.
